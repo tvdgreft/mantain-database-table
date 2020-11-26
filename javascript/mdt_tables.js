f7ade0799dfaf4f82a45863edad0cd2ce99b94d0
tvdgreft <tvdgreft@kabelfoon.nl>
2020-11-27 00:17:03 +0100

    Add files via upload

---
+++ javascript/mdt_tables.js
@@ -0,0 +1,30 @@
+$(document).ready(HideDetail);
+$(document).ready(ShowDetail);
+$(document).ready(OnPopover);
+
+/*
+	Extra row after each row in a table with detail information (display all fields)  about the row
+	
+*/
+function HideDetail()
+{
+	$('.showdetail').closest('tr').hide();
+}
+function ShowDetail()
+{
+	$('.showrecord').on('click', function()
+	{
+		$(this).closest('tr').next().toggle();
+	});
+}
+/*
+  popover a text
+  */
+function OnPopover()
+{
+  $('[data-toggle="popover"]').popover(
+  {
+	  html:true,
+	  container: 'body'
+  });
+}
